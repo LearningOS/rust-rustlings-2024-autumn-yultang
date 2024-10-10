171cf97ea06718e41e79f71fb13fa4b9db8d9e9a
yultang <yultang@ruc.edu.cn>
2024-10-10 20:10:34 +0800

    solve structs

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -5,13 +5,19 @@
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+// 两种不同的结构体定义方式
+
+// 传统的C结构体定义方式
 struct ColorClassicStruct {
     // TODO: Something goes here
+    red: u8,
+    green: u8,
+    blue: u8,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+// 元组结构体定义方式
+struct ColorTupleStruct(u8, u8, u8/* TODO: Something goes here */);
 
 #[derive(Debug)]
 struct UnitLikeStruct;
@@ -24,6 +30,11 @@ mod tests {
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
         // let green =
+        let green = ColorClassicStruct{
+                red: 0,
+                green: 255,
+                blue: 0,
+        };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -34,6 +45,7 @@ mod tests {
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
         // let green =
+        let green = ColorTupleStruct(0, 255, 0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -44,6 +56,7 @@ mod tests {
     fn unit_structs() {
         // TODO: Instantiate a unit-like struct!
         // let unit_like_struct =
+        let unit_like_struct = UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
