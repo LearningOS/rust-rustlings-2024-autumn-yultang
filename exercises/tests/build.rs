51fcc52c328ce81b7e3c5c7289860f49c9019e83
yultang <yultang@ruc.edu.cn>
2024-10-14 11:20:19 +0800

    solve tests and lifetimes

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -1,15 +1,23 @@
 //! This is the build script for both tests7 and tests8.
 //!
 //! You should modify this file to make both exercises pass.
+//! 
+
+use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
     // In tests7, we should set up an environment variable
     // called `TEST_FOO`. Print in the standard output to let
     // Cargo do it.
-    let timestamp = std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
         .unwrap()
         .as_secs(); // What's the use of this timestamp here?
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
+
+    println!("cargo:rustc-cfg=feature=\"pass\"");
+
+    /* 
     let your_command = format!(
         "Your command here with {}, please checkout exercises/tests/build.rs",
         timestamp
@@ -21,4 +29,5 @@ fn main() {
     // Cargo about that.
     let your_command = "Your command here, please checkout exercises/tests/build.rs";
     println!("cargo:{}", your_command);
+    */
 }
